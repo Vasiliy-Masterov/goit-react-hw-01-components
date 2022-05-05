1b239ea8d5c674ebbac703d969cfc57069d58206
Vasiliy-Masterov <vasiliy.masterov@gmail.com>
2022-05-05 13:29:26 +0300

    create components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+//import { App } from 'components/App';
+//import { Test } from 'components/Test';
+import { Profile } from 'components/Social-profile';
+import { Statistics } from 'components/Statistics';
+import { FriendList } from 'components/Friend-list';
+import { TransactionHistory } from 'components/Transaction-history';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Profile />
+    <Statistics />
+    <FriendList />
+    <TransactionHistory />
   </React.StrictMode>
 );
