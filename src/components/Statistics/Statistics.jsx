aa428251672b480787781f05e3d821e0e09ae951
Vasiliy-Masterov <vasiliy.masterov@gmail.com>
2022-05-12 16:55:28 +0300

    update

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,29 +1,23 @@
 //import PropTypes from 'prop-types';
-//import data from './data.json';
-//import style from './Statistics.module.css';
+import stats from './data.json';
+import style from './Statistics.module.css';
+
+const title = "Upload stats";
 
 export const Statistics = () => {
-  //const { title, stats } = data;
+  
   return (
-    <section className="statistics">
-      <h2 className="title">Upload stats</h2>
-      <ul className="stat-list">
-        <li className="item">
-          <span className="label">.docx</span>
-          <span className="percentage">4%</span>
-        </li>
-        <li className="item">
-          <span className="label">.mp3</span>
-          <span className="percentage">14%</span>
-        </li>
-        <li className="item">
-          <span className="label">.pdf</span>
-          <span className="percentage">41%</span>
-        </li>
-        <li className="item">
-          <span className="label">.mp4</span>
-          <span className="percentage">12%</span>
-        </li>
+    <section className={style.statistics}>
+      {title.length > 0 ? < h2 className={style.title}>{title}</h2> : null}
+      <ul className={style.stat_list}>
+        {stats.map(item => (
+            <li key={item.id} className={style.item}>
+              <span className={style.label}>{item.label}</span>
+              <span className={style.percentage}>{item.percentage}</span>
+            </li>
+            )
+          )
+        }
       </ul>
     </section>
   );
